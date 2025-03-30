c46e9b2502c4adbf46e44c3b17f6a4a8032072e7
akane9506 <akane19950611@gmail.com>
2025-03-29 22:29:00 -0400

    base url fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/youtube_draw/",
+  base: "/youtube-draw/",
   plugins: [react()],
   resolve: {
     alias: {
