9ee0fc1d9de771b058267076277ca3b692ccc2ad
akane9506 <akane19950611@gmail.com>
2025-04-07 20:32:26 -0400

    comment date filter updated

--- src/components/routes/results/CommentFilters.tsx
+++ src/components/routes/results/CommentFilters.tsx
@@ -1,5 +1,61 @@
-const CommentFilters = () => {
-  return <div>Filters</div>;
+import { DateRangePicker, type RangeValue } from "@heroui/react";
+import { parseDate, type CalendarDate } from "@internationalized/date";
+
+const getDateString = (date: Date) => {
+  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+};
+
+type CommentFiltersProps = {
+  min: number;
+  max: number;
+  updateDateRange: (newDateRange: number[]) => void;
+};
+
+const CommentFilters = ({ min, max, updateDateRange }: CommentFiltersProps) => {
+  const minDate = new Date(min);
+  const maxDate = new Date(max);
+
+  const minDateString = getDateString(minDate);
+  const maxDateString = getDateString(maxDate);
+
+  // update the date range state
+  const handleDateRangeChange = (value: RangeValue<CalendarDate> | null) => {
+    if (!value) {
+      console.log("No date range selected");
+      return;
+    }
+    const startDate = value.start;
+    const endDate = value.end;
+    const startTime = new Date(
+      startDate.year,
+      startDate.month - 1,
+      startDate.day
+    ).getTime();
+    // should add 23:59 to the end date to include the whole day
+    const endTime = new Date(
+      endDate.year,
+      endDate.month - 1,
+      endDate.day,
+      23,
+      59
+    ).getTime();
+    updateDateRange([startTime, endTime]);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <DateRangePicker
+        aria-label="Date Range Picker"
+        label="Comment Date"
+        className="max-w-xs"
+        labelPlacement="inside"
+        minValue={parseDate(minDateString)}
+        maxValue={parseDate(maxDateString)}
+        defaultValue={{ start: parseDate(minDateString), end: parseDate(maxDateString) }}
+        onChange={handleDateRangeChange}
+      />
+    </div>
+  );
 };
 
 export default CommentFilters;
--- src/components/routes/results/Comments.tsx
+++ src/components/routes/results/Comments.tsx
@@ -1,27 +1,47 @@
-import { useContext, useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { Tabs, Tab, Pagination } from "@heroui/react";
 import { SearchResultContext } from "@/contexts/SearchResultContext";
 import { PreferenceContext } from "@/contexts/PreferenceContext";
 import { CONTENTS } from "@/consts";
 import CommentItem from "@/components/routes/results/CommentItem";
-import CommentFilters from "./CommentFilters";
+import CommentFilters from "@/components/routes/results/CommentFilters";
 
 const Comments = () => {
   const { language, commentsPerPage } = useContext(PreferenceContext);
   const { searchResults } = useContext(SearchResultContext);
   const [page, setPage] = useState<number>(1);
 
-  // const [filters, setFilters] = useState<string[]>([]);
+  const [minTime, maxTime] = useMemo(() => {
+    const dates = searchResults.map((data) => data.createdAt);
+    return [Math.min(...dates), Math.max(...dates)];
+  }, [searchResults]);
+
+  // this date range state should be initialized with the min and max time
+  const [dateRange, setDateRange] = useState<number[]>([minTime, maxTime]);
+
+  const handleUpdateDateRange = (newDateRange: number[]) => {
+    setDateRange(newDateRange);
+  };
+
+  const filteredResults = useMemo(() => {
+    return searchResults.filter((data) => {
+      return data.createdAt >= dateRange[0] && data.createdAt <= dateRange[1];
+    });
+  }, [searchResults, dateRange]);
 
   return (
-    <div className="flex-1 w-full py-4 flex justify-center overflow-auto">
-      <CommentFilters />
-      <div className="flex flex-col items-center px-40">
+    <div className="flex-1 w-full py-4 grid grid-cols-5 overflow-auto max-h-full">
+      <CommentFilters
+        min={minTime}
+        max={maxTime}
+        updateDateRange={handleUpdateDateRange}
+      />
+      <div className="col-span-4 flex flex-col items-center overflow-y-auto">
         <Tabs classNames={{ panel: "flex-1 flex h-[80%]" }} size="md" radius="full">
           <Tab key="comments" title={CONTENTS.tab[language][0]}>
             <div className="flex flex-col gap-4 items-center">
               <div className="flex-1 overflow-y-auto px-5">
-                {searchResults
+                {filteredResults
                   .slice((page - 1) * commentsPerPage, page * commentsPerPage)
                   .map((comment) => (
                     <CommentItem
@@ -33,7 +53,7 @@ const Comments = () => {
               <Pagination
                 color="secondary"
                 page={page}
-                total={Math.ceil(searchResults.length / commentsPerPage)}
+                total={Math.ceil(filteredResults.length / commentsPerPage)}
                 onChange={setPage}
                 variant="light"
                 size="sm"
