77647700f3914dafab8df90c0620349273d6db0d
akane9506 <akane19950611@gmail.com>
2025-04-12 10:53:33 -0400

    result page refactored

---
+++ src/components/routes/results/Users.tsx
@@ -0,0 +1,14 @@
+import { User } from "@/models";
+import UserAvatar from "@/components/routes/results/UserAvatar";
+
+const Users = ({ filteredUsers }: { filteredUsers: User[] }) => {
+  return (
+    <div className="grid grid-cols-6 gap-y-6 overflow-y-auto mx-auto px-10 py-4">
+      {filteredUsers.map((user) => (
+        <UserAvatar key={user.displayName} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default Users;
