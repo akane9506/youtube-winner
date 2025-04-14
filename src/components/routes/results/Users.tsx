eef95c69e988fc86e1bb9d93f34633322847ec17
akane9506 <akane19950611@gmail.com>
2025-04-14 12:15:37 -0400

    grid layout updated

--- src/components/routes/results/Users.tsx
+++ src/components/routes/results/Users.tsx
@@ -3,7 +3,7 @@ import UserAvatar from "@/components/routes/results/UserAvatar";
 
 const Users = ({ filteredUsers }: { filteredUsers: User[] }) => {
   return (
-    <div className="grid grid-cols-6 gap-y-6 overflow-y-auto mx-auto px-10 py-4">
+    <div className="grid grid-cols-6 h-fit gap-y-6 overflow-y-auto mx-auto px-10 py-4">
       {filteredUsers.map((user) => (
         <UserAvatar key={user.displayName} user={user} />
       ))}
