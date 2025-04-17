5de342a647b869911fca3d91ea593f2fbf732f7e
akane9506 <akane19950611@gmail.com>
2025-04-16 23:51:38 -0400

    draw function updated

--- src/components/routes/draw/TierDraw.tsx
+++ src/components/routes/draw/TierDraw.tsx
@@ -3,10 +3,14 @@ import { SearchResultContext } from "@/contexts/SearchResultContext";
 import { User } from "@/models";
 import TierSection from "@/components/routes/draw/TierSection";
 
+const DELAY = 1000;
+
 type TierDrawProps = {
   drawState: number;
   updateDrawState: (newState: number) => void;
 };
+
+const timeouts: (NodeJS.Timeout | undefined)[] = [undefined, undefined, undefined];
 
 const TierDraw = ({ drawState, updateDrawState }: TierDrawProps) => {
   const { users } = useContext(SearchResultContext);
@@ -15,16 +19,32 @@ const TierDraw = ({ drawState, updateDrawState }: TierDrawProps) => {
   const [silverWinners, setSilverWinners] = useState<(User | undefined)[]>([undefined]);
   const [bronzeWinners, setBronzeWinners] = useState<(User | undefined)[]>([undefined]);
 
+  // sample the winners
   const sampling = useCallback(() => {
     const indices = Array.from({ length: users.length }, (_, i) => i);
     const shuffledIndices = indices.sort(() => Math.random() - 0.5);
-    console.log(shuffledIndices);
-  }, [users]);
+    const starts = [0, goldWinners.length, goldWinners.length + silverWinners.length];
+    const ends = [starts[1], starts[2], starts[2] + bronzeWinners.length];
+
+    const setters = [setGoldWinners, setSilverWinners, setBronzeWinners];
+
+    for (let i = 0; i < 3; i++) {
+      if (timeouts[i]) clearTimeout(timeouts[i]);
+      timeouts[i] = setTimeout(
+        () => {
+          setters[i](() => {
+            return shuffledIndices.slice(starts[i], ends[i]).map((index) => users[index]);
+          });
+        },
+        (i + 1) * DELAY
+      );
+    }
+  }, [users, goldWinners, silverWinners, bronzeWinners]);
 
   useEffect(() => {
     if (drawState === 1) {
       sampling();
-      updateDrawState(0);
+      updateDrawState(2);
     }
   }, [drawState, sampling, updateDrawState]);
 
